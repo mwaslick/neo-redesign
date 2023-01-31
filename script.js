c1779457b417f810c3a46116a3ff29bf60100ccf
Maria Waslick <mariawaslick@gmail.com>
2023-01-30 23:00:06 -0500

    added news section

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.setInterval(ut, 1000);
 
 function ut() {
   var d = new Date();
-  document.getElementById("time").innerHTML = d.toLocaleTimeString();
-  document.getElementById("date").innerHTML = d.toLocaleDateString();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' });;
+  document.getElementById("date").innerHTML = d.toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles' });;
 }
 
 ut();
