9610eca612d8e7dd521a4e427ec2b091ec4655f4
Maria Waslick <mariawaslick@gmail.com>
2023-02-02 22:15:28 -0500

    added inbox design

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+const list = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+list.map((el) => {
+  let opts = {
+    animation: false,
+  }
+  if (el.hasAttribute('data-bs-content-id')) {
+    opts.content = document.getElementById(el.getAttribute('data-bs-content-id')).innerHTML;
+    opts.html = true;
+  }
+  new bootstrap.Popover(el, opts);
+})
+
+
 window.setInterval(ut, 1000);
 
 function ut() {
